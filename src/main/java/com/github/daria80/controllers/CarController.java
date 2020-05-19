d1de881eaf60aec94ca26889943b3bf90bd648f6
smirnova <dddsssmmm555@mail.ru>
2020-05-19 23:21:24 +0300

    add program

--- src/main/java/com/github/daria80/controllers/CarController.java
+++ src/main/java/com/github/daria80/controllers/CarController.java
@@ -4,8 +4,12 @@ import com.github.daria80.models.Car;
 import com.github.daria80.repositories.CarRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,7 +18,13 @@ public class CarController {
 
     private final CarRepository carRepository;
 
+    @PostMapping("/cars")
     public Car createCar(@RequestBody Car car) {
         return  carRepository.save(car);
+    }
+
+    @GetMapping("/cars")
+    public List<Car> findAll() {
+        return carRepository.findAll();
     }
 }
